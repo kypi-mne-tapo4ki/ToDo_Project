1a1021e7352dc43d763400edc629ae1dfecb08b7
Dmitrii Ermolaev <d.l.ermolaev@yandex.ru>
2023-06-02 23:31:00 +0200

    Add new test in tests.py

--- server/tests.py
+++ server/tests.py
@@ -10,3 +10,7 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 404
 
+@pytest.mark.asyncio
+async def test_read_item():
+    response = await client.get("/api/todo")
+    assert response.status_code == 200
